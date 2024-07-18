d44cde25bedc02ba167824791198b256de24278e
Kofi Otuo <otuokofi@outlook.com>
2024-07-18 01:25:12 +0000

    first commit

--- src/build.rs
+++ src/build.rs
@@ -1,6 +1,9 @@
+use std::path::PathBuf;
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let path = PathBuf::from("src").join("hello_world.capnp");
     capnpc::CompilerCommand::new()
-        .file("src\\hello_world.capnp")
+        .file(path)
         .run()?;
     Ok(())
 }
--- src/infrastructure/capnp_rpc/client.rs
+++ src/infrastructure/capnp_rpc/client.rs
@@ -1,18 +1,30 @@
 use crate::hello_world_capnp::hello_world;
 use capnp_rpc::{rpc_twoparty_capnp, twoparty, RpcSystem};
-use std::net::ToSocketAddrs;
+use std::marker::PhantomData;
+use std::net::{SocketAddr, ToSocketAddrs};
 
 use futures::AsyncReadExt;
+use tokio::runtime::Builder;
+use tokio::sync::oneshot;
+use tokio::task::LocalSet;
 
-pub async fn run_client(  message: String ) ->Result<String, Box<dyn std::error::Error>> {
-    let server_addr : String = "127.0.0.1:4000".to_string();
-    let addr = server_addr
-        .to_socket_addrs().unwrap()
-        .next()
-        .expect("could not parse address");       
-         
-         rocket::tokio::task::LocalSet::new()
-            .run_until( async move {
+pub struct RpcClient {
+    receiver: oneshot::Receiver<String>,
+}
+
+impl RpcClient {
+    fn new(message: String) -> Self {
+        let (sender, receiver) = oneshot::channel();
+        let rt = Builder::new_current_thread().enable_all().build().unwrap();
+        let server_addr: String = "127.0.0.1:4000".to_string();
+        let addr = server_addr
+            .to_socket_addrs()
+            .unwrap()
+            .next()
+            .expect("could not parse address");
+        std::thread::spawn(move || {
+            let local = LocalSet::new();
+            local.spawn_local(async move {
                 let stream = rocket::tokio::net::TcpStream::connect(&addr).await?;
                 stream.set_nodelay(true).unwrap();
                 let (reader, writer) =
@@ -23,20 +35,34 @@ pub async fn run_client(  message: String ) ->Result<String, Box<dyn std::error:
                     rpc_twoparty_capnp::Side::Client,
                     Default::default(),
                 ));
-                let mut rpc_system = RpcSystem::new(rpc_network, None);
+                let rpc_system = RpcSystem::new(rpc_network, None);
+                let mut rpc_system = rpc_system;
                 let hello_world: hello_world::Client =
                     rpc_system.bootstrap(rpc_twoparty_capnp::Side::Server);
-    
                 rocket::tokio::task::spawn_local(rpc_system);
-                //handle.spawn(rpc_system);
-                println!("entro ");
+
                 let mut request = hello_world.say_hello_request();
                 request.get().init_request().set_name(&message[..]);
-    
                 let reply = request.send().promise.await?;
-                println!("se fue 2");
-                let reply_message  = reply.get()?.get_reply()?.get_message()?.to_str()?;
+                let reply_message = reply.get()?.get_reply()?.get_message()?.to_str()?;
                 println!("received: {}", reply_message);
-                Ok(reply_message.to_string())
-            }).await
+                //send the message to the receiver
+                sender.send(reply_message.to_string()).unwrap();
+                Ok::<(), Box<dyn std::error::Error>>(())
+            });
+            rt.block_on(local);
+        });
+        Self { receiver }
+    }
+
+    async fn get_response(mut self) -> Result<String, String> {
+        match self.receiver.await {
+            Ok(response) => Ok(response),
+            Err(error) => Err(error.to_string()),
         }
+    }
+}
+pub async fn run_client(message: String) -> Result<String, String> {
+    let mut client = RpcClient::new(message);
+    client.get_response().await
+}
